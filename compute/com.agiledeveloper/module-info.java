b6957537cdb7e59570b2d0f0dd5dd526b2b4d816
Erik Weibust <erik@weibust.net>
2019-02-02 15:33:16 -0600

    changes for Part 3

---
+++ compute/com.agiledeveloper/module-info.java
@@ -0,0 +1,9 @@
+// contextual keyword
+module com.agiledeveloper.computation {
+    exports com.agiledeveloper.compute;
+}
+
+// module always requires other modules and exports its own packages.
+// requires modules, exports packages
+
+//Reuse-release equivalency principle
