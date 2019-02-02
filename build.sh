7e0d9c2eff664a19691eed92e7ac8ba26fb54e3f
Erik Weibust <erik@weibust.net>
2019-02-02 14:35:14 -0600

    cleaned up build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ mkdir -p output/mlib;
 mkdir -p output/classes;
 
 javac -d output/classes `find compute -name *.java`;
-jar -v -c -f output/mlib/compute.jar -C output/classes .;
+jar -c -f output/mlib/compute.jar -C output/classes .;
 
 /bin/rm -rf output/classes/*;
 
